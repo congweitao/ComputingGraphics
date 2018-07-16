b36c13f05628407afcf0b6b5ed417c808ce49a21
congweitao <congweitao@github.com>
2018-07-16 17:45:45 +0800

    add two sum

---
+++ TwoSum.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+public int[] two_sum(int[] nums, int target) {
+    int[] result = new int[2];
+    map<int, int> map = new HashMap<int, int>();
+    
+
+}
