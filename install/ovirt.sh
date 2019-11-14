3b139fc5cda13c63080502aa942855b09efb9373
congweitao <congweitao@hotmail.com>
2019-11-14 05:41:26 -0500

    add ovirt

--- install/ovirt.sh
+++ install/ovirt.sh
@@ -3,4 +3,6 @@
 rpm -ivh https://cbs.centos.org/kojifiles/packages/rh-postgresql10/3.1/1.bs1.el7/aarch64/rh-postgresql10-runtime-3.1-1.bs1.el7.aarch64.rpm
 rpm -ivh http://mirror.centos.org/altarch/7/sclo/aarch64/rh/rh-postgresql10/rh-postgresql10-postgresql-libs-10.6-1.el7.aarch64.rpm
 rpm -ivh http://mirror.centos.org/altarch/7/sclo/aarch64/rh/rh-postgresql10/rh-postgresql10-postgresql-10.6-1.el7.aarch64.rpm
+rpm -ivh http://mirror.centos.org/altarch/7/sclo/aarch64/rh/rh-postgresql10/rh-postgresql10-postgresql-contrib-10.6-1.el7.aarch64.rpm
+rpm -ivh http://mirror.centos.org/altarch/7/sclo/aarch64/rh/rh-postgresql10/rh-postgresql10-postgresql-server-10.6-1.el7.aarch64.rpm
 
