0bebf1b60b374509cf8c16c0d35926938311c78c
congweitao <congweitao@hotmail.com>
2019-12-26 10:28:55 -0500

    add aarch64 from ovirt

---
+++ install/autoinstall_ovirt.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+http://mirror.centos.org/altarch/7/extras/aarch64/Packages/centos-release-ovirt43-1.0-1.el7.centos.noarch.rpm
+http://mirror.centos.org/altarch/7/extras/aarch64/Packages/centos-release-opstools-1-8.el7.noarch.rpm
+http://mirror.centos.org/altarch/7/extras/aarch64/Packages/centos-release-qemu-ev-1.0-4.el7.centos.noarch.rpm
+http://mirror.centos.org/altarch/7/extras/aarch64/Packages/centos-release-virt-common-1-1.el7.centos.noarch.rpm
+http://mirror.centos.org/altarch/7/extras/aarch64/Packages/centos-release-gluster6-1.0-1.el7.centos.noarch.rpm
--- install/ovirt-server.sh
+++ install/ovirt-server.sh
@@ -86,6 +86,16 @@ su - postgres -c "source /opt/rh/rh-postgresql10/enable;psql -d template1 -c \"c
 su - postgres -c "source /opt/rh/rh-postgresql10/enable;psql -d engine -c \"CREATE EXTENSION \\\"uuid-ossp\\\";\""
 --> OK
 
+# make rpm components
+wget https://cbs.centos.org/kojifiles/packages/python-ovirt-engine-sdk4/4.2.4/1.el7/aarch64/python-ovirt-engine-sdk4-4.2.4-1.el7.aarch64.rpm
+wget https://resources.ovirt.org/pub/ovirt-4.3/src/ovirt-imageio-common/ovirt-imageio-common-1.5.2.tar.gz
+yum install python-webob qemu-img-rhev
+--> OK
+
+# install collectd
+5.8.1-3
+--> OK
+
 # for ovirt-host-deploy
 wget https://github.com/oVirt/ovirt-host-deploy/archive/ovirt-host-deploy-1.8.4.tar.gz
 autoreconf -ivf
