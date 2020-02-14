05c85bce161f16ff701f0a8090e13ccb49a2ecdd
congweitao <congweitao@hotmail.com>
2020-02-13 20:43:42 -0500

    demo

--- hdc_codelab/openssl-demo.c
+++ hdc_codelab/openssl-demo.c
@@ -25,20 +25,35 @@ int main(int argc, char **argv)
     // ENGINE_ctrl_cmd_string(e, "KAE_CMD_ENABLE_ASYNC", "1", 0);
     
     RSA *rsa = RSA_new_method(e); // 指定引擎用于RSA加解密
-  
-   FILE* fp = NULL;
-   if ((fp = fopen((PUBLIC_KEY), "r")) == NULL) { 
+
+   /* the user code */  
+   FILE* fp1 = NULL, *fp2 = NULL;
+   if ((fp1 = fopen((PUBLIC_KEY), "r")) == NULL) { 
         printf("public key open failed.\n");
         return;
    }
    
-   if ((rsa = PEM_read_RSAPublicKey(fp, NULL, NULL, NULL)) == NULL) {
-        printf("rsd key read failed!\n");
+   if ((rsa = PEM_read_RSA_PUBKEY(fp1, NULL, NULL, NULL)) == NULL) {
+        printf("pub key read failed!\n");
         return;
    }
 
-    RSA_print_fp(stdout, rsa, 0);    
+   RSA_print_fp(stdout, rsa, 0);    
 
-    ENGINE_free(e);
-    fclose(fp);
+   if ((fp2 = fopen((PRIVATE_KEY), "r")) == NULL) { 
+        printf("private key open failed.\n");
+        return;
+   }
+
+   if ((rsa = PEM_read_RSAPrivateKey(fp2, NULL, NULL, NULL)) == NULL) {
+        printf("private key read failed!\n");
+        return;
+   }
+
+   RSA_print_fp(stdout, rsa, 0);    
+
+   fclose(fp1);
+   fclose(fp2);
+
+   ENGINE_free(e);
 }
